29f3948044044a22fedeaad8678101757a7af095
Hamish Starling <hamish.starling@softwire.com>
2023-07-06 12:11:25 +0100

    Version 2 with use dynamite if at least 3 available

--- src/bots/v.ts
+++ src/bots/v.ts
@@ -3,6 +3,25 @@ import { Gamestate, BotSelection } from '../models/gamestate';
 const MAX_DYNAMITE = 100;
 const MAX_TURNS = 2500;
 const PTS_TO_WIN = 1000;
+const USE_DYNAMITE_THRESH = 3;
+
+declare global {
+    interface Array<T> {
+        takeWhile(fn: (a: T) => boolean): Array<T>;
+    }
+}
+
+Array.prototype.takeWhile = function<T>(fn: (a: T) => boolean) {
+    let results: T[] = [];
+    for (const t of this) {
+        if (fn(t)) {
+            results.push(t);
+        } else {
+            break;
+        }
+    }
+    return results;
+}
 
 // Excluding maximum
 function getRandomInt(max: number) {
@@ -16,27 +35,15 @@ function getRandomChoice(list: any[]) {
 class Bot {
     private dyn_turns: number[]; // turn indices where we will use dynamite
     private turn_number = 0;
-    private dyn_pointer = 0;     // points to next dynamite usage
+    private dyn_used = 0;     // points to next dynamite usage
 
-    constructor() {
-        // Populate dynamite turns
-        this.dyn_turns = [];
-        for (let i = 0; i < MAX_DYNAMITE; ++i) {
-            let next = getRandomInt(PTS_TO_WIN);
-            while (this.dyn_turns.includes(next)) { // ensure distinct turns
-                next = getRandomInt(PTS_TO_WIN);
-            }
-            this.dyn_turns.push(next);
-        }
-
-        // Get them in order so pointer method works
-        this.dyn_turns.sort((n1,n2) => n1 - n2);
+    getTurnScore(gamestate: Gamestate) {
+        return gamestate.rounds.reverse().takeWhile(({p1: p1, p2: p2}) => p1 == p2).length + 1;
     }
 
     makeMove(gamestate: Gamestate): BotSelection {
-        this.turn_number++;
-        if (this.turn_number == this.dyn_turns[this.dyn_pointer]) { // planned to use dynamite
-            this.dyn_pointer++;
+        if (this.getTurnScore(gamestate) >= USE_DYNAMITE_THRESH && this.dyn_used < MAX_DYNAMITE) {
+            this.dyn_used++;
             return 'D';
         } else {
             return getRandomChoice(['R','P','S']) // otherwise don't use dynamite
